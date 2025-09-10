187230f81ee8b94e7870ba8949dd365ef8b87652
Jolenemel <jolenemelanson33@gmail.com>
2025-09-10 15:55:30 -0400

    Starting notes for the tool

---
+++ bonetool.lua
@@ -0,0 +1,20 @@
+--Bone highlight tool
+-- I want this to be able to highlight multiple bones, similar to how a lasso tool would highlight points but for bones.
+-- will need to make a bone-highlight tool mainly made for creating a unified bone strength, saving the bones as a 
+-- grouping to be named and stored in the bone ui folders to have globally within moho to be used on any project on moho
+
+--need to create a simple ui component for re-usable bones 
+--it will have a default folder so every bone set is saved if they do not have one 
+--it will be able to make new folders where you can name the folder and add bones to that folder 
+--it will need to keep the properties of the bone- bone strength, bone names, lk settings, etc. 
+
+--this ui will also be able to keep copies of the bones in this ui component and have it saved no matter if it is a new or old project. 
+--this ui will be able to add any bone selected to the current project layer as long as it is a bone layer
+--this ui can have more bones added to it per specified foler or just defaults to the default bone folder
+
+--problem I am trying to fix 
+--making bones take a long time and I want to make sure that my rigs are up to date and as advanced as possible so I would 
+--want to speed up my ability to make perfect rigs by saving them within moho to re-use for other models 
+
+--I would also love the ability to clone and mirror a bone set. A right arm and a left arm are not much different from one another 
+--so being able to highlight and mirror a bone set would be preferable and helpful for speeding up rigs in moho
